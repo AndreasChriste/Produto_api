95d1246aa38913606667ac0b76c38b63ddb9be5d
Andreas Christe <andreash.christe@hotmail.com>
2023-10-26 20:28:55 -0300

    Add post method

--- src/main/java/com/spring/produtos/controllers/ProductController.java
+++ src/main/java/com/spring/produtos/controllers/ProductController.java
@@ -1,9 +1,18 @@
 package com.spring.produtos.controllers;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.produtos.dtos.ProductRecordDto;
+import com.spring.produtos.model.ProductModel;
 import com.spring.produtos.repositories.ProductRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class ProductController {
@@ -11,4 +20,11 @@ public class ProductController {
 	@Autowired
 	ProductRepository productRepository;
 	
+	
+	@PostMapping("/product")
+	public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto){
+		var productModel = new ProductModel();
+		BeanUtils.copyProperties(productRecordDto, productModel);
+		return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+	}
 }
